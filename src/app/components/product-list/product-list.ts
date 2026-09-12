6ba53eaf645810c0a1210b4a5041cdeeb476cd54
makhafallcisse <makhaa6c@gmail.com>
2026-09-12 20:25:47 +0000

    correction et supression des doublons (crée par erreur )et Ajout du footer

--- src/app/components/product-list/product-list.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  imports: [],
-  selector: 'app-product-list',
-  styleUrl: './product-list.css',
-  templateUrl: './product-list.html',
-})
-export class ProductList {}
